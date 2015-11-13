065956f24faf6b9ec455d5f50ef66b56f0e034fc
PFei-He <498130877@qq.com>
2015-11-13 09:22:50 +0800

    修改版本号

--- PFKit/Vendor/PFKit/PFModel/PFModel.m
+++ PFKit/Vendor/PFKit/PFModel/PFModel.m
@@ -7,7 +7,7 @@
 //
 //  https://github.com/PFei-He/PFKit
 //
-//  vesion: 0.0.1
+//  vesion: 0.0.2
 //
 //  Permission is hereby granted, free of charge, to any person obtaining a copy
 //  of this software and associated documentation files (the "Software"), to deal
